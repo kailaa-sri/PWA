fc1cf394f3bf1e21dbf57cef5e10f233b974817e
kailaa sri <kailaarajendran@gmail.com>
2021-04-10 10:19:20 +0530

    cache update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
     // Perform install steps
     var CACHE_NAME = 'my-site-cache-v1';
     var urlsToCache = [
-
+        './',
         '/src/style.css',
         '/images'
     ];

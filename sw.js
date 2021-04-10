d24762783f540be0a33580f22881c3096e9fc8dc
kailaa sri <kailaarajendran@gmail.com>
2021-04-10 14:26:15 +0530

    pwa

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', function(event) {
-    // Perform install steps
+
     var CACHE_NAME = 'DiagnosticsCache';
     var urlsToCache = [
         './',
@@ -14,7 +14,66 @@ self.addEventListener('install', function(event) {
         })
     );
 });
-var deferredPrompt;
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+    // Optionally, send analytics event that PWA install promo was shown.
+    console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+
+buttonInstall.addEventListener('click', async() => {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+});
+
+window.addEventListener('appinstalled', () => {
+    // Hide the app-provided install promotion
+    hideInstallPromotion();
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null;
+    // Optionally, send analytics event to indicate successful install
+    console.log('PWA was installed');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var deferredPrompt;
 window.addEventListener('beforeinstallprompt', function(event) {
     event.preventDefault();
     deferredPrompt = event;
@@ -34,4 +93,4 @@ btnAdd.addEventListener('click', function(event) {
 
 window.addEventListener('appinstalled', function(e) {
     app.logEvent('a2hs', 'installed');
-});
+});*/

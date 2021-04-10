05fa6b74054027c27c631aa9fe5923331a49fdbd
kailaa sri <kailaarajendran@gmail.com>
2021-04-10 16:07:30 +0530

    install button

--- index.js
+++ index.js
@@ -9,46 +9,45 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
-
+var installed = 0;
 window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
+    installed = 1;
     document.getElementById("promotion").classList.add("hidden");
     // Clear the deferredPrompt so it can be garbage collected
     window.deferredPrompt = null;
 });
+if (installed == 0) {
+    window.addEventListener('beforeinstallprompt', (event) => {
+        console.log('👍', 'beforeinstallprompt', event);
+        // Stash the event so it can be triggered later.
+        window.deferredPrompt = event;
+        // Remove the 'hidden' class from the install button container
 
-window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('👍', 'beforeinstallprompt', event);
-    // Stash the event so it can be triggered later.
-    window.deferredPrompt = event;
-    // Remove the 'hidden' class from the install button container
+        document.getElementById("promotion").classList.remove("hidden");
+    });
 
-    document.getElementById("promotion").classList.remove("hidden");
-});
 
-document.getElementById("Install").addEventListener('click', async() => {
-    console.log('👍', 'butInstall-clicked');
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-        // The deferred prompt isn't available.
-        return;
-    }
-    // Show the install prompt.
-    promptEvent.prompt();
-    // Log the result
-    const result = await promptEvent.userChoice;
-    console.log('👍', 'userChoice', result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
-    // Hide the install button.
 
-    document.getElementById("promotion").classList.add("hidden");
-});
 
-window.addEventListener('appinstalled', (event) => {
-    console.log('👍', 'appinstalled', event);
-    document.getElementById("promotion").classList.add("hidden");
-    // Clear the deferredPrompt so it can be garbage collected
-    window.deferredPrompt = null;
-});
+
+    document.getElementById("Install").addEventListener('click', async() => {
+        console.log('👍', 'butInstall-clicked');
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+            // The deferred prompt isn't available.
+            return;
+        }
+        // Show the install prompt.
+        promptEvent.prompt();
+        // Log the result
+        const result = await promptEvent.userChoice;
+        console.log('👍', 'userChoice', result);
+        // Reset the deferred prompt variable, since
+        // prompt() can only be called once.
+        window.deferredPrompt = null;
+        // Hide the install button.
+
+        document.getElementById("promotion").classList.add("hidden");
+    });
+}

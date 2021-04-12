1a1a13302f544a87ee6b0cad3a1e72f6e2aa15bb
kailaa sri <kailaarajendran@gmail.com>
2021-04-12 21:20:19 +0530

    cpu utilization chart

--- index.js
+++ index.js
@@ -82,7 +82,32 @@ window.onload = function() {
             .style("height", (d) => (d * 100 + "px"))
 
     }
-
+    const config = {
+        type: 'line',
+        data,
+        options: {}
+    };
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ];
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    };
+    var myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
 
 
 

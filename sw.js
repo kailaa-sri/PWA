258091864411fd444a42f75c750d5264a1bbd4ad
kailaa sri <kailaarajendran@gmail.com>
2021-04-12 16:24:06 +0530

    message passing added

--- sw.js
+++ sw.js
@@ -32,16 +32,14 @@ self.addEventListener('activate', (event) => {
     // Tell the active service worker to take control of the page immediately.
     self.clients.claim();
 });
-chrome.runtime.connect.addListener(function(port) {
-    console.assert(port.name == "knockknock");
-    port.onMessage.addListener(function(msg) {
-        if (msg.joke == "Knock knock")
-            port.postMessage({ question: "Who's there?" });
-        else if (msg.answer == "Madame")
-            port.postMessage({ question: "Madame who?" });
-        else if (msg.answer == "Madame... Bovary")
-            port.postMessage({ question: "I don't get it." });
-    });
+
+var port = chrome.runtime.onConnect({ name: "knockknock" });
+port.postMessage({ joke: "Knock knock" });
+port.onMessage.addListener(function(msg) {
+    if (msg.question == "Who's there?")
+        port.postMessage({ answer: "Madame" });
+    else if (msg.question == "Madame who?")
+        port.postMessage({ answer: "Madame... Bovary" });
 });
 
 self.addEventListener('fetch', function(event) {

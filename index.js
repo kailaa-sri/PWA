1fd4d22b3e4c94851525dfa5d5879e65d28e7c58
kailaa sri <kailaarajendran@gmail.com>
2021-04-13 06:43:31 +0530

    cpu utilization chart

--- index.js
+++ index.js
@@ -73,36 +73,80 @@ window.onload = function() {
 
 
 
-    const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-    ];
-    const data = {
 
-        datasets: [{
-            label: 'Utilization',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: dataset,
-        }]
-    };
+
+
+
+
+
     const config = {
         type: 'line',
-        data,
-        options: {}
+        data: {
+            datasets: [{
+                borderColor: Utils.CHART_COLORS.red,
+                borderWidth: 1,
+                radius: 0,
+                data: data,
+            }]
+        },
+        options: {
+            animation,
+            interaction: {
+                intersect: false
+            },
+            plugins: {
+                legend: false
+            },
+            scales: {
+                x: {
+                    type: 'linear'
+                }
+            }
+        }
     };
 
 
-    var myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
+    const totalDuration = 10000;
+    const delayBetweenPoints = totalDuration / data.length;
+    const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+    const animation = {
+        x: {
+            type: 'number',
+            easing: 'linear',
+            duration: delayBetweenPoints,
+            from: NaN, // the point is initially skipped
+            delay(ctx) {
+                if (ctx.type !== 'data' || ctx.xStarted) {
+                    return 0;
+                }
+                ctx.xStarted = true;
+                return ctx.index * delayBetweenPoints;
+            }
+        },
+        y: {
+            type: 'number',
+            easing: 'linear',
+            duration: delayBetweenPoints,
+            from: previousY,
+            delay(ctx) {
+                if (ctx.type !== 'data' || ctx.yStarted) {
+                    return 0;
+                }
+                ctx.yStarted = true;
+                return ctx.index * delayBetweenPoints;
+            }
+        }
+    };
 
 
 
+    const data = [];
+    const data2 = [];
+    let prev = 100;
+    let prev2 = 80;
+    for (let i = 0; i < 1000; i++) {
+        prev += 5 - Math.random() * 10;
+        data.push({ x: i, y: prev });
 
+    }
 }

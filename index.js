ef0fe5948b6f8f302c3a10e43218ce1571f91d11
kailaa sri <kailaarajendran@gmail.com>
2021-04-13 07:47:38 +0530

    pwa successful

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ window.onload = function() {
         d3.select("h4")
             .data(dataset)
             .enter()
-            .append("div").attr('class', 'bar').style("height", (d) => d * 100).style("margin", 1);
+            .append("div").attr('class', 'bar').style("height", (d) => d * 1000000000).style("margin", 1);
 
     }
 

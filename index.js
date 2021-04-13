b9d415b89c1ab3624c9248f9acc1581fbd7ec1d8
kailaa sri <kailaarajendran@gmail.com>
2021-04-13 06:45:23 +0530

    cpu utilization chart

--- index.js
+++ index.js
@@ -77,7 +77,13 @@ window.onload = function() {
 
 
 
+    const data = [];
+    let prev = 100;
+    for (let i = 0; i < 1000; i++) {
+        prev += 5 - Math.random() * 10;
+        data.push({ x: i, y: prev });
 
+    }
 
     const config = {
         type: 'line',
@@ -140,13 +146,5 @@ window.onload = function() {
 
 
 
-    const data = [];
-    const data2 = [];
-    let prev = 100;
-    let prev2 = 80;
-    for (let i = 0; i < 1000; i++) {
-        prev += 5 - Math.random() * 10;
-        data.push({ x: i, y: prev });
 
-    }
 }
